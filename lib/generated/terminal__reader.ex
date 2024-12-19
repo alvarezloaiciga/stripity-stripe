4ffb1cc93bedbca05637ac5a117ce85ba0f7eaf3
Jose Alvarez <alvarezloaiciga@gmail.com>
2024-12-19 14:53:41 -0600

    Revert api version

--- lib/generated/terminal__reader.ex
+++ lib/generated/terminal__reader.ex
@@ -631,7 +631,6 @@ defmodule Stripe.Terminal.Reader do
           )
 
         Stripe.Request.new_request(opts, %{
-          "Stripe-Version" => "2024-09-30.acacia",
           "terminal_collect_inputs_beta" => "v1"
         })
         |> Stripe.Request.put_endpoint(path)
